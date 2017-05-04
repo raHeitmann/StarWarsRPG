b5cd76f30810bf962d76f84181d82b20691ac524
Ryan Arnett <ryanglennarnett@gmail.com>
2017-05-04 14:11:34 -0500

    got a couple objects

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+var character1 = {
+				name:'Obi Wan Kenobi',
+				HP: '180',
+				baseAtkPwr:'8',
+				counterAtk:'17'
+				};
+
+var character2 = {
+				name:'Luke Skywalker',
+				HP: '200',
+				baseAtkPwr:'7',
+				counterAtk:'19'
+				};
+
+var character3 = {
+				name:'Mace Windu',
+				HP: '170',
+				baseAtkPwr:'6',
+				counterAtk:'20'
+				};
+
+var character4 = {
+				name: 'Yoda',
+				HP: '160',
+				baseAtkPwr:'7',
+				counterAtk:'22'
+				};
+
+console.log(character1);
+console.log(character2);
+console.log(character3);
+console.log(character4);
